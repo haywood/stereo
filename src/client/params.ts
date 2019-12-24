3cc0b13fece08bde502bc724c6bdcf6ead8f8cd0
Michael Reed <1062477+haywood@users.noreply.github.com>
2019-12-23 18:30:00 -0600

    restore support for animate and pause time when it's off

--- src/client/params.ts
+++ src/client/params.ts
@@ -1,21 +1,26 @@
 import * as mic from './mic';
 import { Params } from '../core/pipe/types';
-import { Subject, combineLatest, interval } from 'rxjs';
-import { inputs } from './inputs';
+import { Subject, combineLatest, interval, of, EMPTY } from 'rxjs';
+import { inputs, streams } from './inputs';
+import { delayWhen } from 'rxjs/operators';
 
 export const stream = new Subject<Params>();
 const fps = 60;
+let t = 0;
 
 combineLatest(mic.stream, interval(1000 / fps))
-    .subscribe(([{ esong, beat }, i]: [mic.Music, number]) => {
-        stream.next({
-            pipe: inputs.pipe,
-            theta: inputs.theta,
-            h: inputs.h,
-            l: inputs.l,
-            t: i / fps,
-            bpm: beat.bpm,
-            ebeat: beat.e,
-            esong: esong,
-        });
+    .subscribe(([{ esong, beat }]: [mic.Music, number]) => {
+        if (inputs.animate) {
+            stream.next({
+                pipe: inputs.pipe,
+                theta: inputs.theta,
+                h: inputs.h,
+                l: inputs.l,
+                t,
+                bpm: beat.bpm,
+                ebeat: beat.e,
+                esong: esong,
+            });
+            t += 1 / fps;
+        }
     });
