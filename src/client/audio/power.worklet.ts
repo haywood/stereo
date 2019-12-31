707edc1ed7e3e3a685c72c60a990930c780cdc07
Michael Reed <1062477+haywood@users.noreply.github.com>
2019-12-30 18:03:26 -0600

    slightly more sophisticated chroma calculation

--- src/client/audio/power.worklet.ts
+++ src/client/audio/power.worklet.ts
@@ -1,4 +1,4 @@
-import { chromaCount, binCount } from './constants';
+import { chromaCount, binCount, octaveCount } from './constants';
 import assert from 'assert';
 import { Spectrum } from './spectrum';
 
@@ -13,11 +13,31 @@ class Processor extends AudioWorkletProcessor {
         const frames = inputs.map(channels => channels[0]);
         const powers = this.spectrum.process(frames);
         const power = powers.reduce((t, p) => t + p, 0) / powers.length;
-        const chroma = Spectrum.chroma(argmax(powers)) / (chromaCount - 1);
+        const chroma = this.chroma(powers);
 
         this.port.postMessage({ power, chroma });
 
         return true;
+    };
+
+    /**
+     * Find the index of the maximal power and map it into color
+     * space ([0, 1]) by sending it to a segment of the interval
+     * based on its chroma, and then offsetting it based on its octave.
+     * Notes with the same chroma go to the same color region, and go
+     * further into that region the higher their octave.
+     *
+     * TODO: really would like to base color on a more wholistic picture
+     * of the audio. Probably some kind of rolling average instead of
+     * per-frame max power.
+     */
+    chroma = (powers: number[]) => {
+        const k = argmax(powers);
+        const chroma = Spectrum.chroma(k);
+        const octave = Spectrum.chroma(k);
+        const chromaStep = 1 / chromaCount;
+        const octaveStep = chromaStep / octaveCount;
+        return chroma * chromaStep + octave * octaveStep;
     };
 }
 
