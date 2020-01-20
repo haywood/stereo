90d7634f75132a8138065b773df242cedad0e734
Michael Reed <1062477+haywood@users.noreply.github.com>
2020-01-20 16:06:04 -0600

    use returned position and color in pool

--- src/pipe/pool.ts
+++ src/pipe/pool.ts
@@ -21,12 +21,15 @@ export const runPipeline = async (params: Params) => {
   const n = params.pipe.n;
   const { fn } = new Resolver(params.scope).resolve(params.pipe);
   const buffer = Data.bufferFor(n, 1, fn.d);
+  const data = new Float32Array(buffer);
 
   const size = Math.round(n / poolSize);
   const promises = workers.map(async (w: Remote<Worker>, i) => {
     const offset = i * size;
     const chunk = { offset, size: Math.min(size, n - offset) };
-    await w.iterate(params, chunk, buffer);
+    const { position, color } = await w.iterate(params, chunk, buffer);
+    Data.position(data).set(position, fn.d * offset);
+    Data.color(data).set(color, 3 * offset);
   });
   await Promise.all(promises);
 
