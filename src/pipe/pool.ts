02af55024bc98fecabff7b3cf6f7e25e4f1b24e0
Michael Reed <1062477+haywood@users.noreply.github.com>
2020-01-20 15:39:48 -0600

    remove data cache
    - since there's no separate init step and allocating the buffer
      seems to take negligible time, it's not needed anymore

--- src/pipe/pool.ts
+++ src/pipe/pool.ts
@@ -4,8 +4,6 @@ import { Params } from '../params';
 import { Resolver } from './resolver';
 import { PipelineWorker } from './types';
 
-const data = new Map<string, SharedArrayBuffer>();
-
 export const poolSize = navigator.hardwareConcurrency;
 const workers = new Array<Remote<PipelineWorker>>(poolSize);
 
@@ -19,24 +17,10 @@ export const stopPool = async (): Promise<void> => {
   workers.forEach(w => w[releaseProxy]());
 };
 
-const getKey = (params: Params) =>
-  JSON.stringify({
-    pipe: params.pipe,
-    hv: params.hsv
-  });
-
 export const runPipeline = async (params: Params) => {
-  const key = getKey(params),
-    n = params.pipe.n,
-    cached = data.has(key);
-  let buffer = data.get(key);
-
-  if (!cached) {
-    const resolver = new Resolver(params.scope);
-    const { fn } = resolver.resolve(params.pipe);
-    buffer = Data.bufferFor(n, 1, fn.d);
-    data.set(key, buffer);
-  }
+  const n = params.pipe.n;
+  const { fn } = new Resolver(params.scope).resolve(params.pipe);
+  const buffer = Data.bufferFor(n, 1, fn.d);
 
   const size = Math.round(n / poolSize);
   const promises = workers.map(async (w, i) => {
