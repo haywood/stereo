23716a154be3c4962b4dd6f546d8e334ebc819b4
Michael Reed <1062477+haywood@users.noreply.github.com>
2020-02-08 13:22:42 -0600

    use the middleware so livereload works

--- scripts/dev.js
+++ scripts/dev.js
@@ -2,8 +2,11 @@ const { newBundler, root } = require('./bundler');
 const express = require('express');
 const path = require('path');
 
-newBundler().bundle();
+const bundler = newBundler();
+
+bundler.bundle();
 
 express()
+  .use('/stereo', bundler.middleware())
   .use('/stereo', express.static(path.resolve(root, 'dist')))
   .listen(8080);
