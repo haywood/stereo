992ded2acdb3215f2a3279890a25dfa54807c231
Michael Reed <1062477+haywood@users.noreply.github.com>
2020-02-08 09:50:35 -0600

    debugging pool

--- src/pipe/pool.ts
+++ src/pipe/pool.ts
@@ -1,10 +1,12 @@
 import { releaseProxy, Remote, wrap } from 'comlink';
+import debug from '../debug';
 import { Params } from '../params';
 import { Data } from '../types';
 import { Resolver } from './resolver';
 import { PipeWorker } from './worker';
 
 export const maxPoolSize = navigator.hardwareConcurrency || 2;
+debug('maxPoolSize', maxPoolSize);
 const workers: Remote<PipeWorker>[] = [];
 
 function adjustPoolSize(targetSize: number) {
@@ -15,12 +17,15 @@ function adjustPoolSize(targetSize: number) {
   while (workers.length > targetSize) {
     workers.pop()[releaseProxy]();
   }
+
+  debug('poolSize', workers.length);
+  debug('targetPoolSize', targetSize);
 }
 
 export const runPipeline = async (params: Params) => {
   const resolver = new Resolver(params.scope);
   const n = resolver.resolve(params.pipe.n, 'number');
-  const chunkCount = Math.min(maxPoolSize, Math.max(1, Math.floor(n / 16_000)));
+  const chunkCount = Math.min(maxPoolSize, Math.max(1, Math.floor(n / 2_000)));
   const size = Math.round(n / chunkCount);
 
   adjustPoolSize(chunkCount);
