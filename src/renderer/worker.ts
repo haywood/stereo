b7011667356723b2c790049f36640ba97574160e
Michael Reed <1062477+haywood@users.noreply.github.com>
2020-02-24 08:20:29 -0600

    split lattice into separate func

--- src/renderer/worker.ts
+++ src/renderer/worker.ts
@@ -63,8 +63,7 @@ class Renderer {
       vertexShader: `
       uniform int n;
 
-      void main() {
-        int d = 3;
+      vec3 lattice_01(int d) {
         float branching_factor = round(pow(float(n), 1. / float(d)));
         vec3 x;
 
@@ -73,6 +72,13 @@ class Renderer {
           float dividend = round(position[0] / pow(branching_factor, exp));
           x[k] = float(int(dividend) % int(branching_factor)) / (branching_factor - 1.);
         }
+
+        return x;
+      }
+
+      void main() {
+        int d = 3;
+        vec3 x = lattice_01(d);
 
         vec3 a = vec3(-1.);
         vec3 b = vec3(1.);
