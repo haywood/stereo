06fb27e5b37b87dce0405d4329a2e3248ba97fcd
Michael Reed <1062477+haywood@users.noreply.github.com>
2020-02-28 09:18:36 -0600

    wip: implement cube in glsl

---
+++ src/renderer/shader/glsl/cube.glsl
@@ -0,0 +1,7 @@
+// TODO :D
+// general idea:
+// - Cube has 2*d faces
+// - Each side should have approx equal number of points.
+//
+// With interval as an inspiration, want to write code that indexes into one of
+// the faces, then indexes into a given position within that face.
