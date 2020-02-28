4a2d44d0cd1b1c598bf61d203ee2c82beb9705a1
Michael Reed <1062477+haywood@users.noreply.github.com>
2020-02-28 09:03:22 -0600

    make #defines for the quaternion components

--- src/renderer/shader/glsl/quaternion.glsl
+++ src/renderer/shader/glsl/quaternion.glsl
@@ -1,15 +1,15 @@
-// r = 0
-// i = 1
-// j = 2
-// k = 3
+#define Q_R 0
+#define Q_I 1
+#define Q_J 2
+#define Q_K 3
 
 float[D_MAX] apply_r(int d, float r, float[D_MAX] x) {
   float[D_MAX] y;
 
-  y[0] = x[0] * r; // r * r = r
-  y[1] = x[1] * r; // i * r = i
-  y[2] = x[2] * r; // j * r = j
-  y[3] = x[3] * r; // k * r = k
+  y[Q_R] = x[Q_R] * r; // r * r = r
+  y[Q_I] = x[Q_I] * r; // i * r = i
+  y[Q_J] = x[Q_J] * r; // j * r = j
+  y[Q_K] = x[Q_K] * r; // k * r = k
 
   return y;
 }
@@ -17,10 +17,10 @@ float[D_MAX] apply_r(int d, float r, float[D_MAX] x) {
 float[D_MAX] apply_i(int d, float i, float[D_MAX] x) {
   float[D_MAX] y;
 
-  y[0] = -x[1] * i; // i * i = -1
-  y[1] = x[0] * i; // r * i = i
-  y[2] = x[3] * i; // k * i = j
-  y[3] = -x[2] * i; // j * i = -k
+  y[Q_R] = -x[Q_I] * i; // i * i = -1
+  y[Q_I] = x[Q_R] * i; // r * i = i
+  y[Q_J] = x[Q_K] * i; // k * i = j
+  y[Q_K] = -x[Q_J] * i; // j * i = -k
 
   return y;
 }
@@ -28,10 +28,10 @@ float[D_MAX] apply_i(int d, float i, float[D_MAX] x) {
 float[D_MAX] apply_j(int d, float j, float[D_MAX] x) {
   float[D_MAX] y;
 
-  y[0] = -x[2] * j; // j * j = -1
-  y[1] = -x[3] * j; // k * j = -i
-  y[2] = x[0] * j; // r * j = j
-  y[3] = x[1] * j; // i * j = k
+  y[Q_R] = -x[Q_J] * j; // j * j = -1
+  y[Q_I] = -x[Q_K] * j; // k * j = -i
+  y[Q_J] = x[Q_R] * j; // r * j = j
+  y[Q_K] = x[Q_I] * j; // i * j = k
 
 
   return y;
@@ -40,10 +40,10 @@ float[D_MAX] apply_j(int d, float j, float[D_MAX] x) {
 float[D_MAX] apply_k(int d, float k, float[D_MAX] x) {
   float[D_MAX] y;
 
-  y[0] = -x[3] * k; // k * k = -1
-  y[1] = x[2] * k; // j * k = i
-  y[2] = -x[1] * k; // i * k = -j
-  y[3] = x[0] * k; // r * k = k
+  y[Q_R] = -x[Q_K] * k; // k * k = -1
+  y[Q_I] = x[Q_J] * k; // j * k = i
+  y[Q_J] = -x[Q_I] * k; // i * k = -j
+  y[Q_K] = x[Q_R] * k; // r * k = k
 
 
   return y;
@@ -51,10 +51,10 @@ float[D_MAX] apply_k(int d, float k, float[D_MAX] x) {
 
 float[D_MAX] quaternion(int d, float[4] q, float[D_MAX] x) {
   float[D_MAX] y,
-    r = apply_r(d, q[0], x),
-    i = apply_i(d, q[1], x),
-    j = apply_j(d, q[2], x),
-    k = apply_k(d, q[3], x);
+    r = apply_r(d, q[Q_R], x),
+    i = apply_i(d, q[Q_I], x),
+    j = apply_j(d, q[Q_J], x),
+    k = apply_k(d, q[Q_K], x);
 
   for (int n = 0; n < d; n++) {
     y[n] += r[n] + i[n] + j[n] + k[n];
