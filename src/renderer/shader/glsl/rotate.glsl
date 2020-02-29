6f62bb22b877e9bf0e65187fd6737794a26c1bb3
Michael Reed <1062477+haywood@users.noreply.github.com>
2020-02-29 08:19:14 -0600

    fix rotate and torus

--- src/renderer/shader/glsl/rotate.glsl
+++ src/renderer/shader/glsl/rotate.glsl
@@ -3,4 +3,6 @@ void rotate(int d, float phi, int d0, int d1) {
 
   x[d0] = x0 * r0 - x1 * r1;
   x[d1] = x0 * r1 + x1 * r0;
+
+  copy(x, y);
 }
--- src/renderer/shader/glsl/torus.glsl
+++ src/renderer/shader/glsl/torus.glsl
@@ -7,6 +7,7 @@ void torus(int d, float[D_MAX] r) {
   for (int k = 1; k < d - 1; k++) {
     x[0] += r[k];
     rotate(d, tmp[k], k - 1, k + 1);
+    copy(y, x);
   }
   copy(x, y);
 }
