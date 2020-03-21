681138cd531db48e6afea8fc92195488713df6a9
Michael Reed <1062477+haywood@users.noreply.github.com>
2020-03-21 18:50:01 -0500

    remove round, which is builtin to glsl es300

--- src/renderer/shader/glsl/common.glsl
+++ src/renderer/shader/glsl/common.glsl
@@ -1,14 +1,5 @@
 #define zero(v) v = float[](0., 0., 0., 0., 0., 0., 0., 0., 0., 0.);
 
-float round(float x) {
-  float r = mod(x, 1.);
-  if (r < 0.5) {
-    return floor(x);
-  } else {
-    return ceil(x);
-  }
-}
-
 float norm2(float[D_MAX] x) {
   float sum;
   for (int k = 0; k < D_MAX; k++) {
